0722bbec0a9a7a78f041c40b98dffec0b1ef4185
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-09-23 14:59:46 +0200

    Changing sorted-map to array-map, as it preserves original sorting

--- src/clojure/clojurewerkz/cassaforte/conversion.clj
+++ src/clojure/clojurewerkz/cassaforte/conversion.clj
@@ -327,7 +327,7 @@
                                    (cb/deserialize key-format (:key %)))
                                  list))
              values (reverse (map #(to-plain-hash (or (:columns %) (:value %))) list))]
-         (apply sorted-map (interleave names values)))))
+         (apply array-map (interleave names values)))))
 
   ColumnOrSuperColumn
   (to-plain-hash
