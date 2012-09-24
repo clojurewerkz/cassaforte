1cceadd7b2d5abdd72425d6d8f8cec2ffec9a994
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-09-24 08:27:26 +0200

    Avoid dropping keyspace, as CQL problem is resolved now.

--- test/clojurewerkz/cassaforte/test_helper.clj
+++ test/clojurewerkz/cassaforte/test_helper.clj
@@ -8,10 +8,6 @@
 (defn initialize-cql
   [f]
   (cc/connect! "127.0.0.1")
-  (with-thrift-exception-handling
-    (cql/execute "DROP KEYSPACE CassaforteTest1"))
-  (with-thrift-exception-handling
-    (cql/execute "CREATE KEYSPACE CassaforteTest1 WITH strategy_class = 'SimpleStrategy' AND strategy_options:replication_factor = 1;"))
   (cql/set-keyspace! "CassaforteTest1")
   (f))
 
