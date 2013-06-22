a448c1319be2aefdf969515ab003d2282772768c
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-06-22 13:39:16 +0200

    Release 1.0.0-rc5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/cassaforte "1.0.0-rc5-SNAPSHOT"
+(defproject clojurewerkz/cassaforte "1.0.0-rc5"
   :min-lein-version "2.0.0"
   :description "A Clojure client for Apache Cassandra"
   :url "http://clojurecassandra.info"
