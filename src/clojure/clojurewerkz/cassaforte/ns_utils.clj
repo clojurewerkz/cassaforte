38911691dd1c7fcd3147f4c2d997853ff639bf31
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-07-03 21:52:25 +0200

    Improve query ns docstrings

--- src/clojure/clojurewerkz/cassaforte/ns_utils.clj
+++ src/clojure/clojurewerkz/cassaforte/ns_utils.clj
@@ -1,6 +1,6 @@
 (ns clojurewerkz.cassaforte.ns-utils)
 
-(defn defalias
+(defn alias-var
   "Create a var with the supplied name in the current namespace, having the same
   metadata and root-binding as the supplied var."
   [name ^clojure.lang.Var var]
@@ -8,9 +8,13 @@
                                             (meta name)))
          (when (.hasRoot var) [@var])))
 
+(defmacro defalias
+  [dst src]
+  `(alias-var (quote ~dst) (var ~src)))
+
 (defn alias-ns
   "Alias all the vars from namespace to the curent namespace"
   [ns-name]
   (require ns-name)
   (doseq [[n v] (ns-publics (the-ns ns-name))]
-    (defalias n v)))
+    (alias-var n v)))
--- src/clojure/clojurewerkz/cassaforte/query.clj
+++ src/clojure/clojurewerkz/cassaforte/query.clj
@@ -1,57 +1,74 @@
 (ns clojurewerkz.cassaforte.query
   "Functions for building dynamic CQL queries, in case you feel
    that `cql` namespace is too limiting for you."
+  (:use [clojurewerkz.cassaforte.ns-utils])
   (:require
-   [clojurewerkz.cassaforte.ns-utils :as ns-utils]
    [qbits.hayt.dsl.statement :as statement]
    [qbits.hayt.cql :as cql]))
 
 (doseq [module '(dsl.clause fns utils)]
-  (ns-utils/alias-ns (symbol (str "qbits.hayt." module))))
+  (alias-ns (symbol (str "qbits.hayt." module))))
 
 ;;
 ;; Cassaforte Mods
 ;;
 
-
 (def select-query statement/select)
 
 (defn insert-query
+  "http://cassandra.apache.org/doc/cql3/CQL.html#insertStmt
+
+Takes a table identifier and additional clause arguments:
+* values
+* using"
   [table values & clauses]
   (into {:insert table
          :values values} clauses))
 
 (defn update-query
+  "http://cassandra.apache.org/doc/cql3/CQL.html#updateStmt
+
+Takes a table identifier and additional clause arguments:
+
+* using
+* set-columns
+* where
+* only-if
+* if-not-exists"
   [table set-columns & clauses]
   (into {:update table
          :set-columns set-columns} clauses))
 
-(def delete-query statement/delete)
+(defalias delete-query statement/delete)
 
-(def truncate-query statement/truncate)
-(def drop-keyspace-query statement/drop-keyspace)
-(def drop-table-query statement/drop-table)
-(def drop-index-query statement/drop-index)
-(def create-index-query statement/create-index)
+(defalias truncate-query statement/truncate)
+(defalias drop-keyspace-query statement/drop-keyspace)
+(defalias drop-table-query statement/drop-table)
+(defalias drop-index-query statement/drop-index)
+(defalias create-index-query statement/create-index)
 
-(def create-keyspace-query statement/create-keyspace)
-(def create-table-query statement/create-table)
-(def create-column-family-query statement/create-table)
-(def alter-table-query statement/alter-table)
-(def alter-column-family-query statement/alter-column-family)
-(def alter-keyspace-query statement/alter-keyspace)
+(defalias create-keyspace-query  statement/create-keyspace)
+(defalias create-table-query  statement/create-table)
+(defalias create-column-family-query  statement/create-table)
+(defalias alter-table-query  statement/alter-table)
+(defalias alter-column-family-query  statement/alter-column-family)
+(defalias alter-keyspace-query  statement/alter-keyspace)
 
-(def batch-query statement/batch)
+(defalias batch-query  statement/batch)
 
-(def use-keyspace-query statement/use-keyspace)
+(defalias use-keyspace-query  statement/use-keyspace)
 
-(def grant-query statement/grant)
-(def revoke-query statement/revoke)
-(def create-user-query statement/create-user)
-(def alter-user-query statement/alter-user)
-(def drop-user-query statement/drop-user)
-(def list-users-query statement/list-users)
-(def list-perm-query statement/list-perm)
+(defalias grant-query  statement/grant)
+(defalias revoke-query  statement/revoke)
+(defalias create-user-query statement/create-user)
+(defalias alter-user-query statement/alter-user)
+(defalias drop-user-query statement/drop-user)
+(defalias list-users-query statement/list-users)
+(defalias list-perm-query statement/list-perm)
+
+;;
+;; Overrides
+;;
 
 (defn where
   "Clause: takes a map or a vector of pairs to compose the where
