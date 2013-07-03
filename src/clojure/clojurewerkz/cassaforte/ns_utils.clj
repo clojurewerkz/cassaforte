e8e2db957d8f86ef13ec145a3fc5df011232acf7
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-07-03 15:43:57 +0200

    Fix defalias to support docstrings

--- src/clojure/clojurewerkz/cassaforte/ns_utils.clj
+++ src/clojure/clojurewerkz/cassaforte/ns_utils.clj
@@ -1,7 +1,12 @@
 (ns clojurewerkz.cassaforte.ns-utils)
 
-(defmacro defalias ^:private [sym v]
-  `(intern *ns* (with-meta ~sym (meta ~sym)) (deref ~v)))
+(defn defalias
+  "Create a var with the supplied name in the current namespace, having the same
+  metadata and root-binding as the supplied var."
+  [name ^clojure.lang.Var var]
+  (apply intern *ns* (with-meta name (merge (meta var)
+                                            (meta name)))
+         (when (.hasRoot var) [@var])))
 
 (defn alias-ns
   "Alias all the vars from namespace to the curent namespace"
