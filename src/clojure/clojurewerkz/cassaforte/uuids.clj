108131f7c9173883edf01faba6c7c661faf0513a
Michael Klishin <mklishin@gopivotal.com>
2013-11-04 23:07:41 +0400

    Type hint returned values

--- src/clojure/clojurewerkz/cassaforte/uuids.clj
+++ src/clojure/clojurewerkz/cassaforte/uuids.clj
@@ -3,17 +3,17 @@
   (:import com.datastax.driver.core.utils.UUIDs
            java.util.UUID))
 
-(defn random
+(defn ^UUID random
   "Creates a new random (version 4) UUID."
   []
   (UUID/randomUUID))
 
-(defn time-based
+(defn ^UUID time-based
   "Creates a new time-based (version 1) UUID."
   []
   (UUIDs/timeBased))
 
-(defn start-of
+(defn ^UUID start-of
   "Creates a \"fake\" time-based UUID that sorts as the smallest possible
 version 1 UUID generated at the provided timestamp.
 
@@ -21,7 +21,7 @@ version 1 UUID generated at the provided timestamp.
   [^long timestamp]
   (UUIDs/startOf timestamp))
 
-(defn end-of
+(defn ^UUID end-of
   "Creates a \"fake\" time-based UUID that sorts as the biggest possible
 version 1 UUID generated at the provided timestamp.
 
@@ -29,7 +29,7 @@ version 1 UUID generated at the provided timestamp.
   [^long timestamp]
   (UUIDs/endOf timestamp))
 
-(defn unix-timestamp
+(defn ^long unix-timestamp
   "Return the unix timestamp contained by the provided time-based UUID."
   [^UUID uuid]
   (UUIDs/unixTimestamp uuid))
