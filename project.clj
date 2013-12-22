b050146b40362ff0014f116866bc3509d17d7c54
Michael Klishin <michael@clojurewerkz.org>
2013-12-22 23:13:49 +0400

    Add dependency on cassandra-all :( references #26

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
   :dependencies [[org.clojure/clojure                          "1.5.1"]
                  [cc.qbits/hayt                                "1.4.1"
                   :exclusions [org.flatland/useful]]
-                 [com.datastax.cassandra/cassandra-driver-core "2.0.0-rc2"]]
+                 [com.datastax.cassandra/cassandra-driver-core "2.0.0-rc2"]
+                 ;; for data types conversion. MK.
+                 [org.apache.cassandra/cassandra-all           "2.0.2"]]
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :profiles       {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
