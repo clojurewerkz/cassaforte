ef3e34518d9793fbaf781793ac4057c8523f5647
Alex Petrov <alexp@coffeenco.de>
2013-12-23 22:10:37 +0100

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/cassaforte "1.3.0-beta8"
+(defproject clojurewerkz/cassaforte "1.3.0-beta9-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "A Clojure client for Apache Cassandra"
   :url "http://clojurecassandra.info"
