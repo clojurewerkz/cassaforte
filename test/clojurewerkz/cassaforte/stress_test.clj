4c555ac26d7d2545bb1e4ce251daaf8002318d99
Michael Klishin <michael@clojurewerkz.org>
2014-04-04 23:21:50 +0400

    Run prepared statement inserts with fewer documents
    Prepared statements have a limit in Cassandra 2.0.x.

--- test/clojurewerkz/cassaforte/stress_test.clj
+++ test/clojurewerkz/cassaforte/stress_test.clj
@@ -16,6 +16,15 @@
       (truncate table)
       (println "Inserting " n " documents...")
       (time (insert-batch table docs))
+      (is (= n (perform-count table))))))
+
+(deftest insert-test
+  (doseq [n [10 1000 3000]]
+    (let [table  :users
+          docs   (map (fn [i]
+                        {:name (str "user_" i) :city (str "city_" i) :age (int i)})
+                      (take n (iterate inc 1)))]
+      (truncate table)
       (println "Inserting " n " documents with prepared statement...")
       (time (client/prepared (insert-batch table docs)))
       (is (= n (perform-count table))))))
