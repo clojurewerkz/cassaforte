9e3be65564568b5d4d3ab20c26bce41d3ef0e38d
Michael Klishin <michael@clojurewerkz.org>
2014-09-13 22:49:07 +0400

    2.0.0-beta3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/cassaforte "2.0.0-beta3-SNAPSHOT"
+(defproject clojurewerkz/cassaforte "2.0.0-beta3"
   :min-lein-version "2.4.3"
   :description "A Clojure client for Apache Cassandra"
   :url "http://clojurecassandra.info"
