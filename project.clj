fd6370dd435764500768f2b83aa359b54ca80b2d
Michael Klishin <michael@clojurewerkz.org>
2014-11-24 01:18:08 +0300

    Drop Clojure 1.4 and 1.5

--- project.clj
+++ project.clj
@@ -10,9 +10,7 @@
                  [com.datastax.cassandra/cassandra-driver-dse  "2.1.1"]]
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
-  :profiles       {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-                   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-                   :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
+  :profiles       {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
                    :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
                    :dev {:jvm-opts     ["-Dlog4j.configuration=log4j.properties.unit"
                                         "-Xmx2048m"
@@ -23,7 +21,7 @@
                                         [org.xerial.snappy/snappy-java     "1.1.0.1"]
                                         [org.clojure/tools.trace           "0.7.6"]
                                         [clj-time                          "0.9.0-beta1"]]}}
-  :aliases        {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,1.7:dev,master"]}
+  :aliases        {"all" ["with-profile" "dev:dev,1.7:dev,master"]}
   :test-selectors {:focus   :focus
                    :client  :client
                    :cql     :cql
