84449910dc2a6ce951ba1263c404b0f64931b460
Michael Klishin <michael@clojurewerkz.org>
2014-12-20 12:13:39 +0300

    Move collection column tests

--- test/clojurewerkz/cassaforte/collections_test.clj
+++ test/clojurewerkz/cassaforte/collections_test.clj
@@ -35,4 +35,127 @@
       (is (vector? ml))
       (is (= ["clojure" "cassandra"] ml)))
     (truncate s t)
-    (drop-table s t))))
+    (drop-table s t)))
+
+
+    (deftest test-list-operations
+      (create-table s :users_list
+                    (column-definitions
+                     {:name :varchar
+                      :test_list (list-type :varchar)
+                      :primary-key [:name]}))
+
+      (testing "Inserting"
+        (th/test-combinations
+         (insert s :users_list
+                 {:name "user1"
+                  :test_list ["str1" "str2" "str3"]})
+         (is (= ["str1" "str2" "str3"] (get-in (select s :users_list)
+                                               [0 :test_list])))
+         (truncate s :users_list)))
+
+      (testing "Updating"
+      (th/test-combinations
+       (insert s :users_list
+               {:name "user1"
+                :test_list []})
+       (dotimes [i 3]
+         (update s :users_list
+                 {:test_list [+ [(str "str" i)]]}
+                 (where {:name "user1"})))
+
+       (is (= ["str0" "str1" "str2"] (get-in (select s :users_list)
+                                             [0 :test_list])))
+       (truncate s :users_list)))
+
+    (testing "Deleting"
+      (th/test-combinations
+       (insert s :users_list
+               {:name "user1"
+                :test_list ["str0" "str1" "str2"]})
+       (update s :users_list
+               {:test_list [- ["str0" "str1"]]}
+               (where {:name "user1"}))
+
+       (is (= ["str2"] (get-in (select s :users_list)
+                               [0 :test_list])))))
+
+    (drop-table s :users_list))
+
+    (deftest test-map-operations
+      (create-table s :users_map
+                    (column-definitions
+                     {:name :varchar
+                      :test_map (map-type :varchar :varchar)
+                      :primary-key [:name]}))
+
+      (testing "Inserting"
+        (th/test-combinations
+         (insert s :users_map
+                 {:name "user1"
+                  :test_map {"a" "b" "c" "d"}})
+         (is (= {"a" "b" "c" "d"} (get-in (select s :users_map)
+                                          [0 :test_map])))
+         (truncate s :users_map)))
+
+      (testing "Updating"
+        (th/test-combinations
+         (insert s :users_map
+                 {:name "user1"
+                  :test_map {}})
+         (dotimes [i 3]
+           (update s :users_map
+                   {:test_map [+ {"a" "b" "c" "d"}]}
+                   (where {:name "user1"})))
+
+         (is (= {"a" "b" "c" "d"} (get-in (select s :users_map)
+                                          [0 :test_map])))
+         (truncate s :users_map)))
+      (drop-table s :users_map))
+
+
+    (deftest test-set-operations
+      (create-table s :users_set
+                    (column-definitions
+                     {:name :varchar
+                      :test_set (set-type :varchar)
+                      :primary-key [:name]}))
+
+      (testing "Inserting"
+        (th/test-combinations
+         (insert s :users_set
+                 {:name "user1"
+                  :test_set #{"str1" "str2" "str3"}})
+         (is (= #{"str1" "str2" "str3"} (get-in (select s :users_set)
+                                                [0 :test_set])))
+         (truncate s :users_set)))
+
+
+      (testing "Updating"
+        (th/test-combinations
+         (insert s :users_set
+                 {:name "user1"
+                  :test_set #{}})
+         (dotimes [i 3]
+           (dotimes [_ 2]
+             (update s :users_set
+                     {:test_set [+ #{(str "str" i)}]}
+                     (where {:name "user1"}))))
+
+         (is (= #{"str0" "str1" "str2"} (get-in (select s :users_set)
+                                                [0 :test_set])))
+         (truncate s :users_set)))
+
+      (testing "Deleting"
+        (th/test-combinations
+         (insert s :users_set
+                 {:name "user1"
+                  :test_set #{"str0" "str1" "str2"}})
+         (update s :users_set
+                 {:test_set [- #{"str0" "str1"}]}
+                 (where {:name "user1"}))
+
+         (is (= #{"str2"} (get-in (select s :users_set)
+                                  [0 :test_set])))))
+
+      (drop-table s :users_set)))
