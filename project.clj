d436d604dd7cb9e8d70c526213190914b91e6180
Michael Klishin <michael@clojurewerkz.org>
2015-03-08 22:11:54 +0300

    clj-time 0.9.0

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                          :dependencies [[com.codahale.metrics/metrics-core "3.0.2"]
                                         [org.xerial.snappy/snappy-java     "1.1.1.6"]
                                         [org.clojure/tools.trace           "0.7.8"]
-                                        [clj-time                          "0.9.0-beta1"]]}}
+                                        [clj-time                          "0.9.0"]]}}
   :aliases        {"all" ["with-profile" "dev:dev,1.7:dev,master"]}
   :test-selectors {:focus   :focus
                    :client  :client
