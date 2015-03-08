36bd6e7c778d613f2823863196ab11de465bb630
Michael Klishin <michael@clojurewerkz.org>
2015-03-08 22:19:34 +0300

    Hayt 2.1.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure                          "1.6.0"]
-                 [cc.qbits/hayt                                "2.0.0"]
+                 [cc.qbits/hayt                                "2.1.0"]
                  [com.datastax.cassandra/cassandra-driver-core "2.1.4"]
                  [com.datastax.cassandra/cassandra-driver-dse  "2.1.4"]]
   :source-paths      ["src/clojure"]
