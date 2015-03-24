6ab4d7ec5a9d245a59b1e24b4b588b45f1b6ab33
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-24 19:51:04 +0100

    Remove bin

--- bin/ci.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/pcmanus/ccm.git /tmp/ccm
-cd /tmp/ccm
-sudo ./setup.py install
-mkdir -p /tmp/cassandra-data/
-ccm create test -n 1 -s -i 127.0.0. -b -v 2.1.3 --config-dir=/tmp/cassandra-data
