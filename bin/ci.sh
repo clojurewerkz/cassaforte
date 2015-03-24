c5c76ffa495032db8486cf3579c99e797480e4ee
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-24 10:15:45 +0100

    How about https?

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,4 +1,4 @@
-git clone git://github.com/pcmanus/ccm.git /tmp/ccm
+git clone https://github.com/pcmanus/ccm.git /tmp/ccm
 cd /tmp/ccm
 sudo ./setup.py install
 ccm create test -n 1 -s -i 127.0.0. -b -v 2.1.3 --config-dir=/tmp/cassandra-data
