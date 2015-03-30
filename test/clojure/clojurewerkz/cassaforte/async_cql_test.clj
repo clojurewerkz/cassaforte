84c5059c2ff1ed1b13c5cc32a65f1a7fbd592daa
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-30 16:24:50 +0200

    Fix async tests

--- test/clojure/clojurewerkz/cassaforte/async_cql_test.clj
+++ test/clojure/clojurewerkz/cassaforte/async_cql_test.clj
@@ -7,94 +7,94 @@
             [clojure.test                        :refer :all]
             [clojurewerkz.cassaforte.query       :refer :all]))
 
-(let [s (th/make-test-session)]
-  (use-fixtures :each (fn [f]
-                        (th/with-temporary-keyspace s f)))
+(use-fixtures :each (fn [f]
+                      (th/with-temporary-keyspace f)))
 
+(let [s (th/make-test-session)]
   (deftest test-insert
-    (th/test-combinations
-     (let [r {:name "Alex" :city "Munich" :age (int 19)}]
-       @(insert-async s :users r)
-       (is (= r (first @(select-async s :users))))
-       (truncate s :users))))
+    (let [r {:name "Alex" :city "Munich" :age (int 19)}]
+      (insert-async s :users r)
+      (is (= r (first @(select-async s :users))))
+      (truncate s :users)))
+
+  (deftest test-insert-time-unit
+    (let [r {:name "Alex" :city "Munich" :age (int 19)}]
+      (insert-async s :users r)
+      (is (= r (first (deref (select-async s :users) 1 java.util.concurrent.TimeUnit/SECONDS))))
+      (truncate s :users)))
 
   (deftest test-insert-batch-with-ttl
-    (th/test-combinations
-     (let [input [[{:name "Alex" :city "Munich" :age (int 19)} (using :ttl (int 350))]
-                  [{:name "Alex" :city "Munich" :age (int 19)} (using :ttl (int 350))]]]
-       @(insert-batch-async s :users input)
-       (is (= (first (first input)) (first @(select-async s :users))))
-       (truncate s :users))))
+    (let [input [[{:name "Alex" :city "Munich" :age (int 19)} (using :ttl (int 350))]
+                 [{:name "Alex" :city "Munich" :age (int 19)} (using :ttl (int 350))]]]
+      (insert-batch-async s :users input)
+
+      @(insert-batch-async s :users input)
+      (is (= (first (first input)) (first @(select-async s :users))))
+      (truncate s :users)))
 
   (deftest test-insert-batch-plain
-    (th/test-combinations
-     (let [input [{:name "Alex" :city "Munich" :age (int 19)}
-                  {:name "Alex" :city "Munich" :age (int 19)}]]
-       @(insert-batch-async s :users input)
-       (is (= (first input) (first @(select-async s :users))))
-       (truncate s :users))))
+    (let [input [{:name "Alex" :city "Munich" :age (int 19)}
+                 {:name "Alex" :city "Munich" :age (int 19)}]]
+      @(insert-batch-async s :users input)
+      (is (= (first input) (first @(select-async s :users))))
+      (truncate s :users)))
 
   (deftest test-update
     (testing "Simple updates"
-      (th/test-combinations
-       (let [r {:name "Alex" :city "Munich" :age (int 19)}]
-         @(insert-async s :users r)
-         (is (= r (first @(select-async s :users))))
-         (update s :users
-                 {:age (int 25)}
-                 (where {:name "Alex"}))
-         (is (= {:name "Alex" :city "Munich" :age (int 25)}
-                (first @(select-async s :users)))))))
+      (let [r {:name "Alex" :city "Munich" :age (int 19)}]
+        @(insert-async s :users r)
+        (is (= r (first @(select-async s :users))))
+        (update s :users
+                {:age (int 25)}
+                (where {:name "Alex"}))
+        (is (= {:name "Alex" :city "Munich" :age (int 25)}
+               (first @(select-async s :users))))))
 
     (testing "One of many update"
-      (th/test-combinations
-       (dotimes [i 3]
-         (insert-async s :user_posts {:username "user1"
-                                      :post_id (str "post" i)
-                                      :body (str "body" i)}))
-       (update-async s :user_posts
-                     {:body "bodynew"}
-                     (where {:username "user1"
-                             :post_id "post1"}))
-       (is (= "bodynew"
-              (get-in @(select-async s :user_posts
-                                     (where {:username "user1"
-                                             :post_id "post1"}))
-                      [0 :body]))))))
+      (dotimes [i 3]
+        (insert-async s :user_posts {:username "user1"
+                                     :post_id (str "post" i)
+                                     :body (str "body" i)}))
+      (update-async s :user_posts
+                    {:body "bodynew"}
+                    (where {:username "user1"
+                            :post_id "post1"}))
+      (is (= "bodynew"
+             (get-in @(select-async s :user_posts
+                                    (where {:username "user1"
+                                            :post_id "post1"}))
+                     [0 :body])))))
 
   (deftest test-delete
-    (th/test-combinations
-     (->> (range 0 3)
-          (map #(insert-async s :users {:name (str "name" %) :age (int %)}))
-          (map deref)
-          doall) ;; Better deref before couting, otherwise there's a good chance it's not even inserted yet
+    (->> (range 0 3)
+         (map #(insert-async s :users {:name (str "name" %) :age (int %)}))
+         (map deref)
+         doall) ;; Better deref before couting, otherwise there's a good chance it's not even inserted yet
 
-     (is (= 3 (perform-count s :users)))
-     @(delete-async s :users
+
+    (is (= 3 (perform-count s :users)))
+    @(delete-async s :users
                    (where {:name "name1"}))
-     (is (= 2 (perform-count s :users)))
-     (truncate s :users))
+    (is (= 2 (perform-count s :users)))
+    (truncate s :users)
 
-    (th/test-combinations
-     @(insert-async s :users {:name "name1" :age (int 19)})
-     @(delete-async s :users
-                    (columns :age)
-                    (where {:name "name1"}))
-     (is (nil? (:age @(select-async s :users))))
-     (truncate s :users)))
+    @(insert-async s :users {:name "name1" :age (int 19)})
+    @(delete-async s :users
+                   (columns :age)
+                   (where {:name "name1"}))
+    (is (nil? (:age @(select-async s :users))))
+    (truncate s :users))
 
   (deftest test-insert-with-timestamp
-    (th/test-combinations
-     (let [r {:name "Alex" :city "Munich" :age (int 19)}]
-       @(insert-async s :users r
-                      (using :timestamp (.getTime (java.util.Date.))))
-       (is (= r (first @(select-async s :users))))
-       (truncate s :users))))
+    (let [r {:name "Alex" :city "Munich" :age (int 19)}]
+      @(insert-async s :users r
+                     (using :timestamp (.getTime (java.util.Date.))))
+      (is (= r (first @(select-async s :users))))
+      (truncate s :users)))
 
   (deftest test-select-async-with-callbacks
-    (th/test-combinations
-     (let [r       {:name "Alex" :city "Munich" :age (int 19)}
-           success (fn [res]
-                     (is (= r (first res))))]
-       (is (= [] @(insert-async s :users r)))
-       (client/set-callbacks (select-async s :users) {:success success})))))
+    (let [r       {:name "Alex" :city "Munich" :age (int 19)}
+          success (fn [res]
+                    (is (= r (first res))))]
+      (is (= [] @(insert-async s :users r)))
+      (client/set-callbacks (select-async s :users) {:success success}))))
