52c719000513365c6aba598b051b38ed9dc9a7a8
Alex Petrov <oleksandr.petrov@gmail.com>
2015-03-31 23:04:51 +0200

    Make basic where statements work

--- src/clojure/clojurewerkz/cassaforte/new_query_api.clj
+++ src/clojure/clojurewerkz/cassaforte/new_query_api.clj
@@ -6,23 +6,19 @@
 
 
 
-(defn where
-  [query-builder m]
-
-  )
-
 (defn from
   [table-name]
   [2 (fn from-query [builder]
-       (.from builder table-name))])
+       (.from builder table-name)
+       )])
 
 (defn all
   []
-  [1 (fn all-query [^Select$Selection query-builder] (.all query-builder))])
+  [1 (fn all-query [query-builder] (.all query-builder))])
 
 (defn column
   [column]
-  [1 (fn column-query [^Select$Selection query-builder]
+  [1 (fn column-query [query-builder]
        (.column query-builder column))])
 
 (defn columns
@@ -36,60 +32,77 @@
 ;; Clause eq(String name, Object value)
 (defn- ^Clause eq
   [^String column ^Object value]
-  (QueryBuilder/eq name value))
+  (QueryBuilder/eq column value))
 
 (defn- ^Clause in
   [^String column ^java.util.List values]
-  (QueryBuilder/in name values))
+  (QueryBuilder/in column values))
 
 (defn- ^Clause lt
   [^String column ^Object value]
-  (QueryBuilder/lt name value))
+  (QueryBuilder/lt column value))
 
 (defn- ^Clause gt
   [^String column ^Object value]
-  (QueryBuilder/gt name value))
+  (QueryBuilder/gt column value))
 
 (defn- ^Clause lte
   [^String column ^Object value]
-  (QueryBuilder/lte name value))
+  (QueryBuilder/lte column value))
 
 (defn- ^Clause gte
   [^String column ^Object value]
-  (QueryBuilder/gte name value))
+  (QueryBuilder/gte column value))
+
+(def ^:private query-type-map
+  {:in in
+   :=  eq
+   =   eq
+   :>  gt
+   >   gt
+   :>= gte
+   :<  lt
+   <   lt
+   :<= lte})
 
 (defprotocol WhereBuilder
   (build-where [construct query-builder]))
 
 (extend-protocol WhereBuilder
   clojure.lang.IPersistentVector
-  (build-where [construct query-builder]
+  (build-where [construct ^Select$Where query-builder]
     (reduce
-     (fn [])
-     query-builder)
+     (fn [^Select$Where builder [query-type column value]]
+       (.and builder ((query-type-map query-type) (name column) value)))
+     query-builder
+     construct)
     )
   clojure.lang.IPersistentMap
-  (build-where [construct query-builder]
-    ))
+  (build-where [construct ^Select$Where query-builder]
+    (reduce
+     (fn [^Select$Where builder [column value]]
+       (.and builder (eq (name column) value)))
+     query-builder
+     construct)))
 
 (defn where
   [m]
   [3 (fn where-query [query-builder]
-       ()
-       )]
-  )
+       (build-where m (.where query-builder)))])
 
 (defn select
   [table-name & statements]
-  (let [query-builder (QueryBuilder/select)]
-    (->> (conj statements (from table-name))
-         (sort-by first)
-         (map second)
-         (reduce (fn [builder statement]
-                   (statement builder))
-                 query-builder
-                 )
-         (.toString))))
+  (->> (conj statements (from (name table-name)))
+       (sort-by first)
+       ;; (map println)
+       (map second)
+       (reduce (fn [builder statement]
+                 (println builder statement)
+                 (statement builder))
+               (QueryBuilder/select)
+               )
+       (.toString)
+       ))
 
 
 
