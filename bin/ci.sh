33e1b1f4d664a8663e0c53888857bb5ba5fb1e98
Max Barnash <i.am@anhero.ru>
2015-12-13 08:19:52 +0000

    Remove whitespaces breaking multilining

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,8 +1,8 @@
-make start_one_node_cluster                                 && \ 
-lein2 all do clean, test                                    && \ 
-make stop_cluster                                           && \ 
-make start_one_node_cluster CASSANDRA_VERSION=binary:2.0.13 && \ 
-lein2 test                                                  && \ 
+make start_one_node_cluster                                 && \
+lein2 all do clean, test                                    && \
+make stop_cluster                                           && \
+make start_one_node_cluster CASSANDRA_VERSION=binary:2.0.13 && \
+lein2 test                                                  && \
 make stop_cluster                                           && \
 make start_one_node_cluster CASSANDRA_VERSION=binary:3.0.1  && \
 lein2 test                                                  && \
