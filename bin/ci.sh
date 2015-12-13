e2968b01616240a2ca36e06a10dfc3e409367b0b
Max Barnash <i.am@anhero.ru>
2015-12-13 07:29:55 +0000

    Add 3.0 to CI

--- bin/ci.sh
+++ bin/ci.sh
@@ -3,4 +3,7 @@ lein2 all do clean, test                                    && \
 make stop_cluster                                           && \ 
 make start_one_node_cluster CASSANDRA_VERSION=binary:2.0.13 && \ 
 lein2 test                                                  && \ 
+make stop_cluster                                           && \
+make start_one_node_cluster CASSANDRA_VERSION=binary:3.0.1  && \
+lein2 test                                                  && \
 make stop_cluster
