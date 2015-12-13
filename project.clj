386dc8db053afe5340554956af7542d019a68666
Max Barnash <i.am@anhero.ru>
2015-12-13 06:58:35 +0000

    Bump driver to 2.1.9

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure                          "1.6.0"]
                  [cc.qbits/hayt                                "2.1.0"]
-                 [com.datastax.cassandra/cassandra-driver-core "2.1.6"]
-                 [com.datastax.cassandra/cassandra-driver-dse  "2.1.6"]]
+                 [com.datastax.cassandra/cassandra-driver-core "2.1.9"]
+                 [com.datastax.cassandra/cassandra-driver-dse  "2.1.9"]]
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :profiles       {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
