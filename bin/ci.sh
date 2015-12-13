e6d8e0898e283c7880ac2ffe36f94a13faa6f5f4
Max Barnash <i.am@anhero.ru>
2015-12-13 12:07:18 +0000

    Fix syntax

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,3 +1,3 @@
 make start_one_node_cluster CASSANDRA_VERSION=binary:$CASSANDRA_VERSION && \
 lein2 all do clean, test, clean                                         && \
-make stop_cluster                                                       && \
+make stop_cluster
