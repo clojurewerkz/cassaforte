7e08e08d28df8c16f136aefe4d3a988ff71fa0b1
Max Barnash <i.am@anhero.ru>
2015-12-13 12:05:23 +0000

    Move cassandra versions into matrix

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,9 +1,3 @@
-make start_one_node_cluster                                 && \
-lein2 all do clean, test, clean                             && \
-make stop_cluster                                           && \
-make start_one_node_cluster CASSANDRA_VERSION=binary:2.0.13 && \
-lein2 do test, clean                                        && \
-make stop_cluster                                           && \
-make start_one_node_cluster CASSANDRA_VERSION=binary:3.0.1  && \
-lein2 do test, clean                                        && \
-make stop_cluster
+make start_one_node_cluster CASSANDRA_VERSION=binary:$CASSANDRA_VERSION && \
+lein2 all do clean, test, clean                                         && \
+make stop_cluster                                                       && \
