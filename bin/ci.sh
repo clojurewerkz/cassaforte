6be587c320a01740e9ce4d47d7a7f96e395ab57b
Max Barnash <i.am@anhero.ru>
2015-12-13 08:20:41 +0000

    Clean between tests

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,9 +1,9 @@
 make start_one_node_cluster                                 && \
-lein2 all do clean, test                                    && \
+lein2 all do clean, test, clean                             && \
 make stop_cluster                                           && \
 make start_one_node_cluster CASSANDRA_VERSION=binary:2.0.13 && \
-lein2 test                                                  && \
+lein2 do test, clean                                        && \
 make stop_cluster                                           && \
 make start_one_node_cluster CASSANDRA_VERSION=binary:3.0.1  && \
-lein2 test                                                  && \
+lein2 do test, clean                                        && \
 make stop_cluster
